0c7e9712d4dcd7a544528eb42b59186bce1c9fce
million <61341108+sakanafuto@users.noreply.github.com>
2020-06-30 14:12:29 +0900

    Carrierwaveの導入

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,32 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
+  def size_range
+    1..5.megabytes
+  end
+
+  def store_dir
+    if Rails.env.test?
+      "uploads_#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    else
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+  
+  version :show do
+    process resize_to_fit: [800, 800]
+  end
+
+  version :index do
+    process resize_to_fit: [400, 400]
+  end
+end
