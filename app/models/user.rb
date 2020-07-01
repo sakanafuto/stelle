5d758fb7d7ae14533f6a99920ef1cb0e498b89ea
million <61341108+sakanafuto@users.noreply.github.com>
2020-07-01 15:40:48 +0900

    rspec#2

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,7 @@ class User < ApplicationRecord
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL }
   before_save { self.email = email.downcase }
-  validates :password, presence: true, length: { minimum: 6 }, on: create
+  validates :password, presence: true, length: { minimum: 6 }
   
   # ゲストユーザーとしてログイン
   def self.guest
