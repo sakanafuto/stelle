5ec430f39597da54c2c6bb6a12026d2b0e5d3e5b
million <61341108+sakanafuto@users.noreply.github.com>
2020-07-01 10:41:21 +0900

    annotateの実行

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: prefectures
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Prefecture < ApplicationRecord
   has_many :posts
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id            :bigint           not null, primary key
+#  caption       :text(65535)
+#  image         :string(255)
+#  name          :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  prefecture_id :bigint           not null
+#  user_id       :bigint
+#
+# Indexes
+#
+#  index_posts_on_prefecture_id  (prefecture_id)
+#  index_posts_on_user_id        (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (prefecture_id => prefectures.id)
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :post do
     name { "MyString" }
