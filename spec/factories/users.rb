cbb7f63c44b26a3de9fc15654cada5058db2b2a9
million <61341108+sakanafuto@users.noreply.github.com>
2020-07-14 15:44:45 +0900

    rspec#5

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -46,4 +46,14 @@ FactoryBot.define do
     password { 'guestpass'}
     guest { 'true' }
   end
+
+  trait :with_posts do
+    transient do
+      posts_count { 5 }
+    end
+
+    after(:create) do |user, evaluator|
+      create_list(:post, evaluator.posts_count, user: user)
+    end
+  end
 end
--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -23,4 +23,32 @@ RSpec.describe "Posts", type: :request do
     end
   end
 
+
+  context '都道府県で検索できているか' do
+    # postの作成
+    before do
+      user = create(:user)
+      prefecture = create(:prefecture, name: "愛知県")
+      @post = create(
+        :post,
+        user: user,
+        prefecture: prefecture
+      )
+
+      other_prefecture = create(:prefecture, name: "福井県")
+      @other_post = create(
+        :post,
+        user: user,
+        prefecture: other_prefecture
+      )
+    end
+
+    it '@postを返すこと' do
+      expect(Post.ransack(prefecture_id: @post.prefecture.id)).to be_empty
+    end
+
+    it '@other_postを返さないこと' do
+      expect(Post.ransack(prefecture_id: @other_post.prefecture.id)).to_not be_empty
+    end
+  end
 end
